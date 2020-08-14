c0d6b9f2e59a40983c61cbc552a4accf6779846a
ElladanTasartir <erickmalta100@gmail.com>
2020-08-14 14:30:53 -0300

    feat: :ok_hand: add Profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Main, LeftSide, RightSide } from './styles';
+
+import ProfileData from '../../components/ProfileData';
 
 const Profile: React.FC = () => {
-  return <div />;
-}
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData
+            username={'ElladanTasartir'}
+            name={'Erick Malta'}
+            avatarUrl={
+              'https://avatars0.githubusercontent.com/u/51251287?v=4'
+            }
+            followers={233}
+            following={8}
+            company={'Rocketseat'}
+            location={'São Paulo, Brazil'}
+            email={'erickmalta100@gmail.com'}
+            blog={undefined}
+          />
+        </LeftSide>
+        <RightSide></RightSide>
+      </Main>
+    </Container>
+  );
+};
 
 export default Profile;
